98d4362ba49e59594f9f04f3ad3dee1ef0c8ea6c
Arno Joosen <arno.joosen@outlook.com>
2024-11-21 21:26:55 +0100

    implement add button

--- mauiApp/Pages/TodoPage.xaml.cs
+++ mauiApp/Pages/TodoPage.xaml.cs
@@ -3,8 +3,22 @@ using Backend.ViewModels;
 namespace mauiApp.Pages;
 
 public partial class TodoPage : ContentPage {
-	public TodoPage(TodoViewModel _vm) {
-		BindingContext = _vm;
+
+	TodoViewModel _vm;
+	public TodoPage(TodoViewModel vm) {
+		_vm = vm;
+		BindingContext = vm;
 		InitializeComponent();
+		
 	}
+
+	protected override async void OnAppearing()
+	{
+		_vm.UpdateTodoList();
+	}
+
+    private void Add_Button_Clicked(object sender, EventArgs e)
+    {
+		_vm.addTodo();
+    }
 }
