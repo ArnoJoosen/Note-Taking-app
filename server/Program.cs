624aeae45eeba30ac0e408e981a29a553b5aa554
Arno Joosen <arno.joosen@outlook.com>
2024-12-06 23:48:32 +0100

    add

--- server/Program.cs
+++ server/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string _connectionstring = builder.Configuration["ConnectionStrings:DefaultConnection"];
+string _connectionstring = "server=api-db;database=appdatabase;user=apiuser;password=apipassword;"; //builder.Configuration["ConnectionStrings:DefaultConnection"];
 builder.Services.AddDbContext<SQLContext>(opt => opt.UseMySql(_connectionstring, ServerVersion.AutoDetect(_connectionstring)));
 builder.Services.AddScoped<ITodoRepo, TodoRepo>();
 builder.Services.AddScoped<INoTeRepo, NoteRepo>();
